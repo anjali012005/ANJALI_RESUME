b3ed0f84916fca8fb4b832e512961b270228bbd5
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-02-23 23:13:22 +0530

    new img added

--- frontend/src/Components/Projects/Projects.jsx
+++ frontend/src/Components/Projects/Projects.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Projects.css'
 import img1 from '../Assets/experince-img.jpg'
-import img2 from '../Assets/experince-img2.jpg'
+import img2 from '../Assets/easybook.jpg'
+// import img2 from 'frontend/src/assets/easybook.jpg'
 
 const Projects = () => {
     return (
@@ -16,7 +17,21 @@ const Projects = () => {
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">Static Card Title  <a href="#">hey</a></h3>
+                        <h3 className="static-card-title">Static Card Title  <a href="https://easybook.onrender.com/listings">Live</a></h3>
+                        <p className="static-card-description">
+                        EasyBook offers a seamless interface where users can not only discover a diverse array of listings but also contribute by creating their own personalized listings.
+                        </p>
+                    </div>
+                </div>
+
+                <div className='project-card'>
+                    <img
+                        src={img1}
+                        alt="Static Card"
+                        className="static-card-image"
+                    />
+                    <div className="static-card-content">
+                        <h3 className="static-card-title">Static Card Title  <a href="#">Live</a></h3>
                         <p className="static-card-description">
                             This is a description for a static card. The content is hardcoded.
                         </p>
@@ -30,7 +45,7 @@ const Projects = () => {
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">Static Card Title  <a href="#">hey</a></h3>
+                        <h3 className="static-card-title">Static Card Title  <a href="#">Live</a></h3>
                         <p className="static-card-description">
                             This is a description for a static card. The content is hardcoded.
                         </p>
@@ -44,7 +59,7 @@ const Projects = () => {
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">Static Card Title  <a href="#">hey</a></h3>
+                        <h3 className="static-card-title">Static Card Title  <a href="#">Live</a></h3>
                         <p className="static-card-description">
                             This is a description for a static card. The content is hardcoded.
                         </p>
@@ -58,21 +73,7 @@ const Projects = () => {
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">Static Card Title  <a href="#">hey</a></h3>
-                        <p className="static-card-description">
-                            This is a description for a static card. The content is hardcoded.
-                        </p>
-                    </div>
-                </div>
-
-                <div className='project-card'>
-                    <img
-                        src={img1}
-                        alt="Static Card"
-                        className="static-card-image"
-                    />
-                    <div className="static-card-content">
-                        <h3 className="static-card-title">Static Card Title  <a href="#">hey</a></h3>
+                        <h3 className="static-card-title">Static Card Title  <a href="#">Live</a></h3>
                         <p className="static-card-description">
                             This is a description for a static card. The content is hardcoded.
                         </p>    
